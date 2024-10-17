c447cbae6ed9aef19026172988b9f13afcbbafbd
Saif Al-saedi <saif4004@gmail.com>
2024-10-17 12:32:11 -0700

    Rector getAllProperties

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -112,16 +112,48 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+const getAllProperties = function (options, limit = 10) {
+
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += `AND owner_id = $${queryParams.length}`;
+  }
+  if (options.minimum_price_per_night !== null) {
+    queryParams.push(`%${options.minimum_price_per_night * 100}%`);
+    queryString += (`AND cost_per_night >= $${queryParams.length}`);
+ 
+  }
+  if (options.maximum_price_per_night !== null) {
+    queryParams.push(`%${options.maximum_price_per_night * 100}%`);
+    queryString += (`AND cost_per_night <= $${queryParams.length}`); 
+  }
+  if (options.minimum_rating) {
+    queryParams.push(`%${options.minimum_rating}%`);
+    queryString += (`AND AVG(property_reviews.rating) >= $${queryParams.length}`); 
+  }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night 
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+}
 
 /**
  * Add a property to the database
